f2b425163ee6b2526747cac22bc492d7ff87f194
roinelson <roi.nelson@curexus.com>
2023-05-24 15:05:07 +0200

    disable test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,6 +1,4 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  return true;
-});
+test('renders learn react link', () => {});
